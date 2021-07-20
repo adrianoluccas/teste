b091a92f511410d363e3cd19ec09e1c0cd526e5a
Adriano Luccas <adrianoluccas@gmail.com>
2021-07-20 09:26:38 -0300

    first

---
+++ src/teste/Principal.java
@@ -0,0 +1,9 @@
+package teste;
+
+public class Principal {
+	public static void main(String[] args) {
+		String data = "3333-22-11";
+		System.out.println(data.substring(8, 10) + "/" + data.substring(5, 7) + "/" + data.substring(0, 4));
+		
+	}
+}
